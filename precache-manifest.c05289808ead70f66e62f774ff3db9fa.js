438930c30945a261139afa8d9a3bc945cef2a8b1
Abayomi Folarin Festus <maicharles001@gmail.com>
2020-03-12 13:56:42 +0530

    Updates

---
+++ precache-manifest.c05289808ead70f66e62f774ff3db9fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fd5ceb5eea98b163369a7f682e079d",
+    "url": "/GuestManagement/index.html"
+  },
+  {
+    "revision": "971c21723e6969ece9f9",
+    "url": "/GuestManagement/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "3b095ae7a595a84d0ef4",
+    "url": "/GuestManagement/static/css/main.257144b0.chunk.css"
+  },
+  {
+    "revision": "971c21723e6969ece9f9",
+    "url": "/GuestManagement/static/js/2.b2b23d61.chunk.js"
+  },
+  {
+    "revision": "aabd4139eb5c5ed0676661f029f60e8a",
+    "url": "/GuestManagement/static/js/2.b2b23d61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b095ae7a595a84d0ef4",
+    "url": "/GuestManagement/static/js/main.41583095.chunk.js"
+  },
+  {
+    "revision": "760c53c6f235fb0701c7",
+    "url": "/GuestManagement/static/js/runtime-main.6d10be25.js"
+  }
+]);
